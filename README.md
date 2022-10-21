57410ae10aac65043665ff625f06768eaff71bb2
OllieM26 <ollie.madelin@gmail.com>
2022-10-21 22:44:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Frontend for the NFT marketplace using Moralis server.
+
+NOTE:
+- Moralis servers are no longer available so this implementation will not work
+- It is only for completeness
---
+++ components/NFTBox.js
@@ -0,0 +1,146 @@
+import { useEffect, useState } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import nftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
+import { Card } from "web3uikit"
+import { ethers } from "ethers"
+import { UpdateListingModal } from "./UpdateListingModal"
+
+const truncateStr = (fullString, stringLength) => {
+    if (fullString.length <= stringLength) {
+        return fullString
+    }
+    const seperator = "..."
+    let seperatorLength = seperator.length
+    const charsToShow = stringLength - seperatorLength
+    const frontChars = Math.ceil(charsToShow / 2)
+    const backChars = Math.floor(charsToShow / 2)
+    return (
+        fullString.substring(0, frontChars) +
+        seperator +
+        fullString.substring(fullString.length - backChars)
+    )
+}
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+    const { isWeb3Enabled } = useMoralis()
+    const [imageURI, setImageURI] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
+    const [showModal, setShowModal] = useState(false)
+    const hideModal = () => setShowModal(false)
+    const dispatch = useNotification()
+
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+        functionName: "tokenURI",
+        params: {
+            tokenId: tokenId,
+        },
+    })
+    const { runContractFunction: buyItem } = useWeb3Contract({
+        abi: nftMarketplaceAbi,
+        contractAddress: marketplaceAddress,
+        functionName: "buyItem",
+        msgValue: price,
+        params: {
+            nftAddress: nftAddress,
+            tokenId: tokenId,
+        },
+    })
+
+    async function updateUI() {
+        // get token URI
+        // using the image tag from URI, get the image
+        const tokenURI = await getTokenURI()
+        console.log("Token URI is", tokenURI)
+        if (tokenURI) {
+            // get the image
+            // IPFS Gateway: server that will return IPFS files from a normal url
+            // This gateway is by the IPFS team
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+            // We could also render the image on our server
+            // Or for testnets & mainnets use moralis server hooks
+            // Or wait for everyone to adopt IPFS
+        }
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const isOwnedByUser = seller === account || seller === undefined
+    const formattedSellerAddress = isOwnedByUser ? "You" : truncateStr(seller || "", 15)
+
+    const handleCardClick = () => {
+        isOwnedByUser
+            ? setShowModal(true)
+            : buyItem({
+                  onError: (error) => console.log(error),
+                  onSuccess: handleBuyItemSuccess,
+              })
+    }
+
+    const handleBuyItemSuccess = async (tx) => {
+        await tx.wait()
+        dispatch({
+            type: "success",
+            message: "Item Bought!",
+            title: "Item Bought",
+            position: "topR",
+        })
+    }
+
+    return (
+        <div>
+            <div>
+                {imageURI ? (
+                    <div>
+                        <UpdateListingModal
+                            isVisible={showModal}
+                            tokenId={tokenId}
+                            marketplaceAddress={marketplaceAddress}
+                            nftAddress={nftAddress}
+                            onClose={hideModal}
+                        />
+                        <Card
+                            title={tokenName}
+                            description={tokenDescription}
+                            onClick={handleCardClick}
+                        >
+                            <div className="p-2">
+                                <div className="flex flex-col items-end gap-2">
+                                    <div>#{tokenId}</div>
+                                    <div className="italic text-sm">
+                                        Owned by {formattedSellerAddress}
+                                    </div>
+                                    <Image
+                                        loader={() => imageURI}
+                                        src={imageURI}
+                                        height="200"
+                                        width="200"
+                                    />
+                                    <div className="font-bold">
+                                        {ethers.utils.formatEther(price)} ETH
+                                    </div>
+                                </div>
+                            </div>
+                        </Card>
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
+}
